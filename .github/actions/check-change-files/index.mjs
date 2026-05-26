2fddf0328b3c0296fb4d7027fdc269d33adf4ef6
istiuak <istiuak.0@gmail.com>
2026-05-27 05:01:25 +0600

    chore: extract change file check into its own action

---
+++ .github/actions/check-change-files/index.mjs
@@ -0,0 +1,6 @@
+import { appendFileSync, readdirSync } from "fs";
+
+const files = readdirSync("change").filter(file => file.endsWith(".json"));
+const has = files.length > 0;
+
+appendFileSync(process.env.GITHUB_OUTPUT, `has=${has}\n`);
