2ff02979b3f641225925afb34f1121a4b4e0c565
istiuak <istiuak.0@gmail.com>
2026-07-01 03:47:37 +0600

    take validator to seprate repo

--- packages/validator/playground/1.ts
+++
@@ -1,6 +0,0 @@
-import { ax, ParseBase } from './../src'
-const schema = ax.string()
-
-const [data, err] = ParseBase(schema, [10])
-
-console.log(data, err)
--- packages/validator/src/index.ts
+++
@@ -1,4 +0,0 @@
-export * from './lib/ax'
-export * from './lib/parse'
-export * from './lib/schema'
-export * from './lib/types'
